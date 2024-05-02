ea5542a60cbe4c3bddbd6392ce6111ed27b74a47
jeanlucascastro <jeanlucascastro@gmail.com>
2024-05-01 23:21:30 -0300

    update

--- src/main/java/com/sociedade/scheduler/repositories/ScheduleRepository.java
+++ src/main/java/com/sociedade/scheduler/repositories/ScheduleRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     @Query("SELECT s FROM Schedule s " +
@@ -17,5 +17,9 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
             "OR s.finalTime BETWEEN :start AND :end)")
     List<Schedule> findOverlappingSchedules(@Param("start") LocalDateTime start,
                                             @Param("end") LocalDateTime end);
+
+
+    List<Schedule> findByExecutorIdAndInitialTime(Long executorId, LocalDate initialTime);
+
 }
 
