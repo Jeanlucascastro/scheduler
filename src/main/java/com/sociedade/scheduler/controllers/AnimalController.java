46b7f85ca0c91a94ab7f242dbaa6173bec24215a
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2024-05-14 18:52:26 -0300

    animal crud complete

---
+++ src/main/java/com/sociedade/scheduler/controllers/AnimalController.java
@@ -0,0 +1,70 @@
+package com.sociedade.scheduler.controllers;
+
+import com.sociedade.scheduler.model.Animal;
+import com.sociedade.scheduler.model.dto.CreateAnimalDTO;
+import com.sociedade.scheduler.model.dto.UpdateAnimalDTO;
+import com.sociedade.scheduler.model.user.User;
+import com.sociedade.scheduler.services.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/animal")
+public class AnimalController {
+
+    @Autowired
+    private AnimalService animalService;
+
+    @PostMapping()
+    public ResponseEntity<Animal> createAnimal(@RequestBody CreateAnimalDTO animalDTO, @AuthenticationPrincipal User user) {
+        Animal createdAnimal = animalService.saveAnimal(animalDTO, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAnimal);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Animal> getAnimalById(@PathVariable("id") Long id) {
+        Animal Animal = animalService.getAnimalById(id);
+        if (Animal != null) {
+            return ResponseEntity.ok(Animal);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllAnimals(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size,
+            @RequestParam(name = "paginated", defaultValue = "true") boolean paginated
+    ) {
+        if (paginated) {
+            Page<Animal> AnimalsPage = animalService.getAllAnimals(page, size);
+            return ResponseEntity.ok(AnimalsPage);
+        } else {
+            return ResponseEntity.ok(animalService.getAllAnimals());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Animal> updateAnimal(
+            @PathVariable("id") Long id,
+            @RequestBody UpdateAnimalDTO updatedAnimal
+    ) {
+        Animal updated = animalService.updateAnimal(id, updatedAnimal);
+        if (updated != null) {
+            return ResponseEntity.ok(updated);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAnimal(@PathVariable("id") Long id) {
+        animalService.deleteAnimal(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/sociedade/scheduler/services/AnimalService.java
+++ src/main/java/com/sociedade/scheduler/services/AnimalService.java
@@ -5,13 +5,14 @@ import com.sociedade.scheduler.model.Company;
 import com.sociedade.scheduler.model.Animal;
 import com.sociedade.scheduler.model.dto.CreateAnimalDTO;
 import com.sociedade.scheduler.model.dto.UpdateAnimalDTO;
+import com.sociedade.scheduler.model.user.User;
 import com.sociedade.scheduler.repositories.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.util.List;
 
 @Service
@@ -23,12 +24,19 @@ public class AnimalService {
     @Autowired
     private CompanyService companyService;
 
-    public Animal saveAnimal(CreateAnimalDTO animalDTO) {
+    public Animal saveAnimal(CreateAnimalDTO animalDTO, User user) {
 
         Company company = this.companyService.findById(animalDTO.companyId());
 
         Animal Animal = new Animal(
-
+            animalDTO.name(),
+                animalDTO.age(),
+                animalDTO.weight(),
+                animalDTO.size(),
+                animalDTO.animalType(),
+                animalDTO.note(),
+                user,
+                company
         );
 
         return this.AnimalRepository.save(Animal);
@@ -51,6 +59,11 @@ public class AnimalService {
         Animal existingAnimal = this.AnimalRepository.findById(id).orElse(null);
         if (existingAnimal != null) {
             existingAnimal.setName(updatedAnimal.name());
+            existingAnimal.setAge(updatedAnimal.age());
+            existingAnimal.setAnimalType(updatedAnimal.animalType());
+            existingAnimal.setWeight(updatedAnimal.weight());
+            existingAnimal.setSize(updatedAnimal.size());
+            existingAnimal.setNote(updatedAnimal.note());
             return this.AnimalRepository.save(existingAnimal);
         }
         return null;
